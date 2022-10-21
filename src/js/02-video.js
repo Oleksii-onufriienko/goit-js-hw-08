a4ea59d1a06d8e27072af42a71dc0f02292a6ca5
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-21 11:06:18 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,9 +2,14 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(JSON.parse(currentTime).seconds);
+}
 
 player.on('timeupdate', time => {
-  console.log(time);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
 });
 
 player.getVideoTitle().then(function (title) {
